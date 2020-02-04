0f2b546c2c3ec0e8c936777bdcf79f3ec5ed435f
Azrielle <azrielllle@gmail.com>
2020-02-04 23:01:56 +0300

    task2 refactoring and restructuring

--- task2/task2.py
+++ task2/task2.py
@@ -1,81 +1,65 @@
 import sys
-# (Bx-Ax)*(Py-Ay) - (By-Ay)*(Px-Ax)
-# (Cx-Bx)*(Py-By) - (Cy-By)*(Px-Bx)
-# (Dx-Cx)*(Py-Cy) - (Dy-Cy)*(Px-Cx)
-# (Ax-Dx)*(Py-Dy) - (Ay-Dy)*(Px-Dx)
+#  вершины идут по часовой стрелке значит  <= 0 принадлежит четырех угольнику > 0 за пределами
+# (Bx-Ax)*(Py-Ay) - (By-Ay)*(Px-Ax) > 0
+# (Cx-Bx)*(Py-By) - (Cy-By)*(Px-Bx) > 0
+# (Dx-Cx)*(Py-Cy) - (Dy-Cy)*(Px-Cx) > 0
+# (Ax-Dx)*(Py-Dy) - (Ay-Dy)*(Px-Dx) > 0
 
-list_first = []
-list_second = []
-const_mn = []
+class QuadRangle(object):
 
-def open_file(in_file_name):
-    f = open(in_file_name, 'r')
-    data = f.read()
-    in_list=[]
+    def __init__(self, in_file_name):
+        with open(in_file_name, 'r') as lines:
+            data = lines.read()
+        data = data.split('\n')
+        data = [i.split(' ') for i in data]
+        data = [ [float(i[0]), float(i[1])] for i in data]
+
+        # так как в формуле поиска принадлежности точки, есть постояные множители(вершины четырех угольника)
+        # чтобы не расчитывать их для каждой точки заново, создадим массив этих значений.
+        # переместим первый элемент в конец списка и поэлементно вычтим.
+        # [[Ax,Ay],[Bx,By],[Cx,Cy],[Dx,Dy]]
+        # [[Bx,By],[Cx,Cy],[Dx,Dy],[Ax,Ay]]
+        data_zero_element_to_end = list(data)
+        data_zero_element_to_end.append(data_zero_element_to_end.pop(0))
+        constant_factor_list = []
+        for i in range(len(data)):
+            constant_factor_list.append([data_zero_element_to_end[i][0]-data[i][0], data_zero_element_to_end[i][1]-data[i][1]])
+
+        self.data = data
+        self.constant_factor_list = constant_factor_list
+
+    def check_point(self,point_E):
+        #  сравнения с вершинами (нет расчетов приоритет выше)
+        for point in self.data:
+            if (point[0] == point_E[0]) & (point[1] == point_E[1]):
+                return 0
+        for i in range(len(self.data)):
+            tmp_var = (self.constant_factor_list[i][0] * (point_E[1] - self.data[i][1])) - (self.constant_factor_list[i][1] * (point_E[0] - self.data[i][0]))
+            # точки заданный по часовой стрелки следовательно если разница > 0 за перимитром, если 0 то на грани 
+            # оба случия определены, дальнешая проверка бесмыслена.
+            if tmp_var > 0:
+                return (3)
+            elif tmp_var == 0:
+                return (1)
+        return (2)
+
+
+def open_file_point(in_file_name_point):
+    with open(in_file_name_point, 'r') as lines:
+            data = lines.read()
     data = data.split('\n')
-    for i in data:
-        in_list.append(i.split(' '))
-    f.close()
-    points = []
-    for point in in_list:
-        points.append([float(point[0]),float(point[1])])
-
-    print(points)
-    for i in range(len(points)-1):
-        const_mn.append([points[i+1][j]-points[i][j] for j in range(2)])
-    const_mn.append([points[0][j]-points[len(points)-1][j] for j in range(2)])
-    print (const_mn[0][0])
+    data = [i.split(' ') for i in data]
+    data = [[float(i[0]), float(i[1])] for i in data]
+    return data
 
 
-    return points
-
-def check_point(pointF,list_point,const_mn):
-    flag = False
-    for point in list_point:
-        if (point[0] == pointF[0]) & (point[1] == pointF[1]):
-            return 0
-    for i in range(4):
-        # print(i)
-        # print(pointF)
-        # print(list_point)
-        # print(const_mn)
-        tmp_var = (const_mn[i][0]*(pointF[1]-list_point[i][1])) - (const_mn[i][1]*(pointF[0]-list_point[i][0]))
-        # print(tmp_var)
-        if tmp_var > 0:
-            return (3)
-        elif tmp_var == 0:
-            flag = True
-    if flag:
-        return 1
-    else:
-        return 2
-
-
-def open_file_point(in_file_name_point,in_coordinati_4ugla):
-    f = open(in_file_name_point, 'r')
-    data = f.read()
-    in_list=[]
-    data = data.split('\n')
-    for i in data:
-        in_list.append(i.split(' '))
-    f.close()
-    for point in in_list:
-        print(check_point([float(point[0]),float(point[1])],in_coordinati_4ugla,const_mn))
-
+def main(*args):
+    my_quad_ragle = QuadRangle(args[0])
+    point_list = open_file_point(args[1])
+    for point in point_list:
+        print(my_quad_ragle.check_point(point))
 
 
 if (__name__ == "__main__"):
-    print ('hello world')
-    print(sys.argv)
-    in_coordinati_4ugla = open_file(sys.argv[1])
-    print(const_mn)
-    in_list_to4ek = open_file_point(sys.argv[2],in_coordinati_4ugla)
-    
-    print(in_coordinati_4ugla)
-    # print(print_nf(percentil(in_list)))
-    # print(print_nf(mediana(in_list)))
-    # print(print_nf(max_value(in_list)))
-    # print(print_nf(min_value(in_list)))
-    # print(print_nf(average_value(in_list)))
-    
-    # print(f'{a:.{2}f}')
+    if len(sys.argv) > 2:
+        main(sys.argv[1],sys.argv[2])
