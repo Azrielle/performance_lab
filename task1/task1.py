303b095b3b7a913d89b3f073c87de7c04559baa8
Azrielle <azrielllle@gmail.com>
2020-02-04 21:12:10 +0300

    task1 refactoring and restructuring

--- task1/task1.py
+++ task1/task1.py
@@ -1,60 +1,59 @@
 import sys
 
-def open_file(in_file_name):
-    f = open(in_file_name, 'r')
-    data = f.read()
-    in_list=[]
-    data = data.split('\n')
-    for i in data:
-        if not(i == ''):
-            in_list.append(int(i))
-    f.close()
-    return in_list
+class DataList(object):
 
-def percentil(in_list,n=90):
-    tmp_len = len(in_list)
-    drob_poradkov_nomer = ((n / 100) * (tmp_len - 1) + 1)
-    tmp_list = in_list
-    tmp_list.sort()
-    tmp_int = int(drob_poradkov_nomer) - 1
-    tmp_ost = drob_poradkov_nomer - int(drob_poradkov_nomer)
-    print(tmp_list)
-    print(drob_poradkov_nomer)
-    print(tmp_int)
-    print(tmp_ost)
-    print(tmp_len)
-    print(tmp_list[tmp_int])
-    return (tmp_list[tmp_int] + (tmp_list[tmp_int + 1] - tmp_list[tmp_int]) * tmp_ost)
+    def __init__ (self,in_file_name):
+        with open(in_file_name, 'r') as lines:
+            data = lines.read()
+        data = data.replace(' ','')
+        data = data.split('\n')
+        data = [int(i) for i in data if not (i=='')]
+        data.sort()
+        self.data = data
 
+    def get_percentil(self,n=90):
+        drob_poradkov_nomer = ((n / 100) * (len(self.data) - 1) + 1)
+        # -1 так как нумерация с 0
+        tmp_n = int(drob_poradkov_nomer) - 1
+        tmp_d = drob_poradkov_nomer - int(drob_poradkov_nomer)
+        return (self.data[tmp_n] + (self.data[tmp_n + 1] - self.data[tmp_n]) * tmp_d)
+    
+    def get_mediana(self):
+        if len(self.data) % 2 == 1:
+            return self.data[(len(self.data) // 2) + 1]
+        return (self.data[(len(self.data) // 2)] + self.data[(len(self.data) // 2) + 1])/2
+    
+    # Так как массив отсортерован конструктором, min и max находятся на концах.
+    def get_max_value(self):
+        return self.data[-1]
 
-def max_value(in_list):
-    return max(in_list)
+    def get_min_value(self):
+        return self.data[0]
 
-def min_value(in_list):
-    return min(in_list)
+    def get_average_value(self):
+        return sum(self.data)/len(self.data)
+    
+    
+# форматирование вывода.
+def print_float_nf(in_numeral,n=2):
+    print(f'{in_numeral:.{n}f}')
+    return 0
 
-def average_value(in_list):
-    return sum(in_list)/len(in_list)
-
-def mediana(in_list):
-    tmp_list = in_list
-    tmp_list.sort()
-    if len(tmp_list) % 2 == 1:
-       return tmp_list[(len(tmp_list) // 2) + 1]
-    return (tmp_list[(len(tmp_list) // 2)] + tmp_list[(len(tmp_list) // 2) + 1])/2
-
-def print_nf(in_numeral,n=2):
-    return (f'{in_numeral:.{n}f}')
+def main(*args):
+    """Программа получает на вход файл с целыми числами(разделитель новая строка)
+    и выводит:  90 перцентиль
+                медиана
+                максимальное значение
+                минимальное значение
+                среднее значение. """ 
+    my_data_list = DataList(args[0])
+    print_float_nf(my_data_list.get_percentil())
+    print_float_nf(my_data_list.get_mediana())
+    print_float_nf(my_data_list.get_max_value())
+    print_float_nf(my_data_list.get_min_value())
+    print_float_nf(my_data_list.get_average_value())
+    return 0
 
 if (__name__ == "__main__"):
-    print ('hello world')
-    print(sys.argv)
-    in_list = open_file(sys.argv[1])
-    
-    print(in_list)
-    print(print_nf(percentil(in_list)))
-    print(print_nf(mediana(in_list)))
-    print(print_nf(max_value(in_list)))
-    print(print_nf(min_value(in_list)))
-    print(print_nf(average_value(in_list)))
-    # print(f'{a:.{2}f}')
+    if len(sys.argv) > 1:
+        main(sys.argv[1])
