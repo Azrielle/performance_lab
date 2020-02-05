836386376fd1ad66fc1b02d3dd29be50bfd1f751
Azrielle <azrielllle@gmail.com>
2020-02-05 07:38:40 +0300

    task3 refactoring and restructuring

--- task3/task3.py
+++ task3/task3.py
@@ -1,36 +1,31 @@
 import sys
 
-def open_five_file(dir_adress):
-    out_list = [0.0]*16
-    for i in range(1,6,1):
-        tmp_list = []
-        f = open(dir_adress + "/Cash%i.txt" %i, 'r')
-        for line in f:
-            tmp_list.append(float(line))
-        f.close()
-        out_list = add_next_list(out_list,tmp_list)
-    return out_list
+class ListBuyer(object):
+    def __init__(self):
+        self.summ_list_buyer = [0]*16
+    
+    def add_next_list(self, next_list):
+        for i in range(len(self.summ_list_buyer)):
+            self.summ_list_buyer[i] = self.summ_list_buyer[i] + next_list[i]
+        print(self.summ_list_buyer)
 
-def add_next_list(in_list,next_list):
-    for i in range(len(in_list)):
-        in_list[i] = in_list[i] + next_list[i]
-    print(in_list)
-    return in_list
+    def get_first_max_interval(self):
+        return (self.summ_list_buyer.index(max(self.summ_list_buyer)) + 1)
+
+def open_five_file(dir_adress, n=5):
+    list_buyer = ListBuyer()
+    for i in range(1, n+1, 1):
+        with open(dir_adress + "/Cash%i.txt" %i, 'r') as lines:
+            data = lines.read()
+        data = data.split('\n')
+        data = [ float(item) for item in data]
+        list_buyer.add_next_list(data)
+    return list_buyer
+
+def main(*args):
+    my_list_buyer = open_five_file(args[0])
+    print(my_list_buyer.get_first_max_interval())
 
 if (__name__ == "__main__"):
-    print ('hello world')
-    print(sys.argv)
-    s1 = "Cash" + "/Cash1.txt"
-    print(s1)
-    sum_list = open_five_file(sys.argv[1])
-    print(sum_list)
-    max_time = max(sum_list)
-    print(sum_list.index(max_time)+1)
-    
-    # print(print_nf(percentil(in_list)))
-    # print(print_nf(mediana(in_list)))
-    # print(print_nf(max_value(in_list)))
-    # print(print_nf(min_value(in_list)))
-    # print(print_nf(average_value(in_list)))
-    
-    # print(f'{a:.{2}f}')
+    if len(sys.argv) > 1:
+        main(sys.argv[1])
